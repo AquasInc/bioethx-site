f6de3225c2e0af78aa55b7b6ab74479565bafa76
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-02-07 13:54:23 -0500

    feat(layout): nav react element

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,30 @@
 import React from "react";
-import { container, mainHeading } from "./layout.module.css";
+import { Link } from "gatsby";
+import {
+  container,
+  mainHeading,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+} from "./layout.module.css";
 
 const Layout = ({ title, heading, children }) => (
   <div className={container}>
     <title>BioethX | {title}</title>
+    <nav>
+      <ul className={navLinks}>
+        <li className={navLinkItem}>
+          <Link to="/" className={navLinkText}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" className={navLinkText}>
+            About
+          </Link>
+        </li>
+      </ul>
+    </nav>
     <main>
       <h1 className={mainHeading}>{heading}</h1>
       {children}
