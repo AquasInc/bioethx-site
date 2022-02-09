4ead8f733e75a6d39698740f318004c5cbdc4e62
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-02-09 16:20:23 -0500

    feat(layout): css module navbar styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,38 +3,42 @@ import { Link } from "gatsby";
 import {
   container,
   mainHeading,
+  navbar,
   navLinks,
+  navLinkItem,
   navLinkText,
 } from "./layout.module.css";
 
 const Layout = ({ title, heading, children }) => (
-  <div className={container}>
+  <div>
     <title>BioethX | {title}</title>
-    <nav>
-      <ul className={navLinks}>
-        <li>
-          <Link to="/" className={navLinkText}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/about" className={navLinkText}>
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to="/service" className={navLinkText}>
-            Service
-          </Link>
-        </li>
-        <li>
-          <Link to="/contact" className={navLinkText}>
-            Contact
-          </Link>
-        </li>
-      </ul>
+    <nav className={navbar}>
+      <div className={container}>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>
+              Home
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/service" className={navLinkText}>
+              Service
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/contact" className={navLinkText}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
-    <main>
+    <main className={container}>
       <h1 className={mainHeading}>{heading}</h1>
       <hr />
       {children}
