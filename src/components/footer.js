e05e0f786691bd8e2060054d652de00af7e7fa03
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-02-10 15:40:07 -0500

    feat(layout): footer component

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { container, footer } from "./footer.module.css";
+
+const Footer = () => (
+  <footer className={footer}>
+    <div className={container}>
+      <p>hello footer</p>
+    </div>
+  </footer>
+);
+
+export default Footer;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Nav from "./nav";
+import Footer from "./footer";
+import { pageContainer, pageWrap } from "./layout.module.css";
 
 const Layout = ({ title, children }) => (
-  <div>
+  <div className={pageContainer}>
     <title>{"BioethX | " + title}</title>
     <Nav />
-    {children}
+    <div className={pageWrap}>{children}</div>
+    <Footer />
   </div>
 );
 
