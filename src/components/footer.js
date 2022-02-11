e1f7d0aefc29162eb2122bf47c03ef984a3eac9d
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-02-11 16:42:04 -0500

    feat(footer): resources links

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,8 @@
-import { Link } from "gatsby";
 import React from "react";
+import { Link } from "gatsby";
+import BioEthxInfoSheet from "../assets/BioEthxInfoSheet.pdf";
+import BenefitsAtAGlance from "../assets/BenefitsAtAGlance.pdf";
+import ManagingHealthcareEthics from "../assets/ManagingHealthcareEthics.pdf";
 import { container, footer, column, linkText } from "./footer.module.css";
 
 const Footer = () => (
@@ -17,7 +20,7 @@ const Footer = () => (
           (301) 654-4000
         </p>
         <p>
-          <strong>Email:</strong>
+          <strong>Address:</strong>
           <br />
           10400 Connecticut Avenue
           <br />
@@ -39,7 +42,49 @@ const Footer = () => (
         </p>
         <p>&copy; 2022 BioEthx™</p>
       </div>
-      <div className={column}></div>
+      <div className={column}>
+        <p>
+          <strong>
+            <a href="http://bioethxcafe.com/" className={linkText}>
+              BioEthx Cafe
+            </a>
+          </strong>
+          <br />
+        </p>
+        <p>
+          <strong>Print & Go Information</strong>
+          <br />
+          <a
+            href={BioEthxInfoSheet}
+            target="_blank"
+            rel="noreferrer"
+            className={linkText}
+          >
+            BioEthx Information Sheet
+          </a>
+          <br />
+          <a
+            href={BenefitsAtAGlance}
+            target="_blank"
+            rel="noreferrer"
+            className={linkText}
+          >
+            Benefits at a glance
+          </a>
+        </p>
+        <p>
+          <strong>White Papers</strong>
+          <br />
+          <a
+            href={ManagingHealthcareEthics}
+            target="_blank"
+            rel="noreferrer"
+            className={linkText}
+          >
+            Managing Healthcare Ethics
+          </a>
+        </p>
+      </div>
     </div>
   </footer>
 );
