378d3d9f3af539582d1a4d345f4aab32dfa3bc88
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-02-11 14:45:33 -0500

    feat(footer): flex-columns

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,45 @@
+import { Link } from "gatsby";
 import React from "react";
-import { container, footer } from "./footer.module.css";
+import { container, footer, column, linkText } from "./footer.module.css";
 
 const Footer = () => (
   <footer className={footer}>
     <div className={container}>
-      <p>&copy; 2022 BioEthx™</p>
+      <div className={column}>
+        <p>
+          <strong>Email:</strong>
+          <br />
+          info@BioEthx.net
+        </p>
+        <p>
+          <strong>Phone:</strong>
+          <br />
+          (301) 654-4000
+        </p>
+        <p>
+          <strong>Email:</strong>
+          <br />
+          10400 Connecticut Avenue
+          <br />
+          Suite 310
+          <br />
+          Kensington, MD 20895
+        </p>
+      </div>
+      <div className={column}>
+        <p>
+          <Link to="#" className={linkText}>
+            Privacy Policy
+          </Link>
+        </p>
+        <p>
+          <Link to="#" className={linkText}>
+            Terms & Conditions
+          </Link>
+        </p>
+        <p>&copy; 2022 BioEthx™</p>
+      </div>
+      <div className={column}></div>
     </div>
   </footer>
 );
