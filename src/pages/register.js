64e3f1fa01c62b8f5d2c587871cd0fa48b0d29fa
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-02-14 15:40:17 -0500

    feat(register): two-column-layout

--- src/pages/register.js
+++ src/pages/register.js
@@ -6,9 +6,13 @@ import "../styles/index.css";
 const RegistrationPage = () => (
   <Layout title="Contact Us">
     <main className="container">
-      <h1 className="main-heading">Register Email</h1>
-      <hr />
-      <Form />
+      <h1 className="main-heading">Register</h1>
+      <div className="two-column-layout">
+        <div className="text-div">
+          <h2>Please register so we can keep you up to date via email.</h2>
+        </div>
+        <Form />
+      </div>
     </main>
   </Layout>
 );
