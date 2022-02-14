89aac78186f4b6e34fc6c87b6d48bf93116a2502
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-02-14 16:01:02 -0500

    fix(register): page component title

--- src/pages/register.js
+++ src/pages/register.js
@@ -4,7 +4,7 @@ import Form from "../components/form";
 import "../styles/index.css";
 
 const RegistrationPage = () => (
-  <Layout title="Contact Us">
+  <Layout title="Register">
     <main className="container">
       <h1 className="main-heading">Register</h1>
       <div className="two-column-layout">
