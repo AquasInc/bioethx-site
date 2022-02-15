1739449e353d8c6e5a82cadc796eb3922e72c71b
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-02-15 15:59:48 -0500

    refactor(register): styles into separate css module

--- src/pages/register.js
+++ src/pages/register.js
@@ -2,13 +2,14 @@ import React from "react";
 import Layout from "../components/layout";
 import Form from "../components/form";
 import "../styles/index.css";
+import { twoColumnLayout, columnDiv } from "../styles/register.module.css";
 
 const RegistrationPage = () => (
   <Layout title="Register">
     <main className="container">
       <h1 className="main-heading">Register</h1>
-      <div className="two-column-layout">
-        <div className="text-div">
+      <div className={twoColumnLayout}>
+        <div className={columnDiv}>
           <h2>Please register so we can keep you up to date via email.</h2>
         </div>
         <Form />
