d774aac8076ebcef14a0ac2f78b49843e009aff8
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-02-18 13:07:44 -0500

    feat(service): MDXRenderer to dynamically render content from markdown files

--- src/pages/service.js
+++ src/pages/service.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 import Layout from "../components/layout";
 import Sidebar from "../components/sidebar";
 import { articleAside, mainArticle } from "../styles/article-aside.module.css";
@@ -9,7 +10,7 @@ class ServicePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      feature: "Overview",
+      feature: "Service",
     };
   }
 
@@ -23,53 +24,10 @@ class ServicePage extends React.Component {
         <div className={articleAside}>
           <Sidebar onClick={this.handleClick} />
           <main className="container">
-            <h1 className="main-heading">Service</h1>
+            <h1 className="main-heading">{this.state.feature}</h1>
             <article className={mainArticle}>
               <hr />
-              <h2>{this.state.feature}</h2>
-              <p>
-                BioEthx™ has conducted research into private sector practices in
-                documenting and managing the progression of bioethics cases in
-                the clinical setting. BioEthx staff has long been involved in
-                the development of software designed to assist ethicists and
-                ethics committees in managing clinical and organizational ethics
-                cases. Accumulated expertise and evolving software delivery
-                methods have now come together to produce the first
-                publicly-available software system dedicated to the task of
-                healthcare ethics consultation.
-              </p>
-              <p>
-                The end result is the BioEthx online service for consult
-                management. The web-based service is designed to support the
-                management of ethics consultations from initial event and
-                information capture through resolution and follow-up evaluation.
-                It provides an effective and easy-to-use tool for managing the
-                workflow of ethics consultations in accordance with established
-                procedures and to fully document case history and the analytical
-                process undertaken to produce the best possible outcome.
-              </p>
-              <h2>Features</h2>
-              <p>
-                The BioEthx™ consult management service provides a centralized
-                repository for recording all of the important information
-                related to an ethics consultation request and the complete
-                process carried out to achieve the most appropriate outcome.
-              </p>
-              <p>
-                With minimal training, hospital and clinic staff can create a
-                record of an expressed concern or a request for action.
-                Thereafter, the system becomes a valuable tool to guide the
-                workflow required to properly handle the issue, supporting the
-                inclusion of additional specialists and promoting collaboration
-                among consultation team members to work towards an optimal
-                outcome.
-              </p>
-              <p>
-                BioEthx is a complete tool that helps healthcare organizations
-                to ensure that appropriate ethics protocols needed to provide
-                quality healthcare and mitigate risks are applied to both
-                clinical and organizational ethics cases.
-              </p>
+              <MDXRenderer>{this.props.data.mdx.body}</MDXRenderer>
             </article>
           </main>
         </div>
@@ -84,6 +42,7 @@ export const query = graphql`
       frontmatter {
         title
       }
+      body
     }
   }
 `;
