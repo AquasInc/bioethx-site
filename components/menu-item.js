33643e8863b84117aae5f93a01136e6174cdaec6
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2023-01-26 17:38:57 -0500

    feat(nav): highlight current page nav link

--- components/menu-item.js
+++ components/menu-item.js
@@ -1,25 +1,35 @@
+import { useRouter } from "next/router";
 import Link from "next/link";
 import styles from "../styles/menu-item.module.css";
 
-const MenuItem = ({ href, text, handleClick }) => (
-  <li className={styles.navLinkItem}>
-    <span onClick={handleClick}>
-      {/^http/.test(href) ? (
-        <a
-          href={href}
-          target="_blank"
-          rel="noreferrer"
-          className={styles.navLinkText}
-        >
-          {text}
-        </a>
-      ) : (
-        <Link href={href} className={styles.navLinkText}>
-          {text}
-        </Link>
-      )}
-    </span>
-  </li>
-);
+const MenuItem = ({ href, text, handleClick }) => {
+  const router = useRouter();
+
+  return (
+    <li className={styles.navLinkItem}>
+      <span onClick={handleClick}>
+        {/^http/.test(href) ? (
+          <a
+            href={href}
+            target="_blank"
+            rel="noreferrer"
+            className={styles.navLinkText}
+          >
+            {text}
+          </a>
+        ) : (
+          <Link
+            href={href}
+            className={`${styles.navLinkText} ${
+              router.asPath === href ? styles.active : ""
+            }`}
+          >
+            {text}
+          </Link>
+        )}
+      </span>
+    </li>
+  );
+};
 
 export default MenuItem;
