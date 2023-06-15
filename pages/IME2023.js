af4e8739873cd6876f8adfae40d565ca33a7be81
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2023-06-15 12:34:40 -0400

    fix(pages): IME2023 heading inteligence -> intelligence

--- pages/IME2023.js
+++ pages/IME2023.js
@@ -5,7 +5,7 @@ export default function IME2023() {
   return (
     <Layout title="IME 2023">
       <div className={styles.container}>
-        <h1 className={styles.mainHeading}>Artificial Inteligence is an Undeniable Reality</h1>
+        <h1 className={styles.mainHeading}>Artificial Intelligence is an Undeniable Reality</h1>
       </div>
    </Layout>
   )
