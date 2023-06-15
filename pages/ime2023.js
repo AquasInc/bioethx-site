44a6aa42503abad76c6ab9e949016785e2ba8485
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2023-06-15 11:53:00 -0400

    feat(pages): ime2023

---
+++ pages/ime2023.js
@@ -0,0 +1,12 @@
+import Layout from '../components/layout';
+import styles from '../styles/Home.module.css';
+
+export default function IME2023() {
+  return (
+    <Layout>
+      <div className={styles.container}>
+        <h1 className={styles.mainHeading}>Artificial Inteligence is an Undeniable Reality</h1>
+      </div>
+   </Layout>
+  )
+}
