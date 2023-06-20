e68a36792c6e7b674f9e57083e70150da38db05f
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2023-06-20 16:15:26 -0400

    fix(toast): js emoji

--- components/toast.js
+++ components/toast.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import styles from '../styles/Toast.module.css';
 
 const Toast = () => (
-    <Link href={"/IME2023"} className={styles.toast}>{'\uF44B'} IME 2023</Link>
+    <Link href={"/IME2023"} className={styles.toast}>{'\uf09f\u918b'} IME 2023</Link>
 );
 
 export default Toast;
